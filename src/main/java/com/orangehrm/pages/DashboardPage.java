8fb5b30778b5544759c471064f2671adef549952
Nurul Arifin <nurularifin837@gmail.com>
2025-03-30 22:57:33 +0700

    Update Dashboard

--- src/main/java/com/orangehrm/pages/DashboardPage.java
+++ src/main/java/com/orangehrm/pages/DashboardPage.java
@@ -3,6 +3,8 @@ package com.orangehrm.pages;
 import com.orangehrm.base.BasePage;
 import com.orangehrm.utils.JavaScriptUtils;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DashboardPage extends BasePage {
@@ -11,6 +13,41 @@ public class DashboardPage extends BasePage {
     private JavaScriptUtils scriptUtils;
 
     // Select WebElement
+    @FindBy(xpath = "//span[@class='oxd-userdropdown-tab']")
+    private WebElement profileName;
+
+    @FindBy(xpath = "//a[text()='Logout']\n")
+    private WebElement logOutButton;
+
+    @FindBy(xpath = "//a[text()='About']")
+    private WebElement aboutButton;
+
+    @FindBy(xpath = "//h6[text()='About']")
+    private WebElement aboutHeaderText;
+
+    @FindBy(xpath = "//button[@class='oxd-dialog-close-button oxd-dialog-close-button-position']")
+    private WebElement closeButton;
+
+    @FindBy(xpath = "//a[text()='Support']")
+    private WebElement supportButton;
+
+    @FindBy(xpath = "//h6[text()='Getting Started with OrangeHRM']")
+    private WebElement gettingStartedText;
+
+    @FindBy(xpath = "//a[text()='Change Password']")
+    private WebElement changePasswordButton;
+
+    @FindBy(xpath = "(//input[@type='password'])[1]")
+    private WebElement currentPassword;
+
+    @FindBy(xpath = "(//input[@type='password'])[2]")
+    private WebElement newPassword;
+
+    @FindBy(xpath = "(//input[@type='password'])[3]")
+    private WebElement confirmPassword;
+
+    @FindBy(xpath = "//span[text()='Passwords do not match']")
+    private WebElement errorConfirmPassword;
 
     // Constructors
     public DashboardPage(WebDriver driver){
@@ -20,6 +57,179 @@ public class DashboardPage extends BasePage {
     }
 
     // Getter
+    public WebElement getErrorConfirmPassword(){
+        customWait.waitForVisibilityOfElement(errorConfirmPassword);
+        return errorConfirmPassword;
+    }
+
+    public WebElement getCurrentPassword(){
+        customWait.waitForVisibilityOfElement(currentPassword);
+        return currentPassword;
+    }
+
+    public WebElement getNewPassword(){
+        customWait.waitForVisibilityOfElement(newPassword);
+        return newPassword;
+    }
+
+    public WebElement getConfirmPassword(){
+        customWait.waitForVisibilityOfElement(confirmPassword);
+        return confirmPassword;
+    }
+
+    public WebElement getChangePasswordButton(){
+        customWait.waitForElementToBeClickable(changePasswordButton);
+        return changePasswordButton;
+    }
+
+    public WebElement getGettingStartedText(){
+        customWait.waitForVisibilityOfElement(gettingStartedText);
+        return gettingStartedText;
+    }
+
+    public WebElement getSupportButton(){
+        customWait.waitForElementToBeClickable(supportButton);
+        return supportButton;
+    }
+
+    public WebElement getCloseButton(){
+        customWait.waitForElementToBeClickable(closeButton);
+        return closeButton;
+    }
+
+    public WebElement getAboutTextHeader(){
+        customWait.waitForVisibilityOfElement(aboutHeaderText);
+        return aboutHeaderText;
+    }
+
+    public WebElement getAboutButton(){
+        customWait.waitForElementToBeClickable(aboutButton);
+        return aboutButton;
+    }
+
+    public WebElement getLogOutButton(){
+        customWait.waitForElementToBeClickable(logOutButton);
+        return logOutButton;
+    }
+
+    public WebElement getProfileName(){
+        customWait.waitForVisibilityOfElement(profileName);
+        return profileName;
+    }
 
     // Page Actions
+    public String readErrorConfirmPassword(){
+        try{
+            return getErrorConfirmPassword().getText();
+        }catch (Exception e){
+            LOGGER.error("Failed to read the error confirm password.", e);
+            throw e;
+        }
+    }
+
+    public void enterCurrentPassword(String currentPassword){
+        try{
+            getCurrentPassword().sendKeys(currentPassword);
+            LOGGER.info("Entered a current password. {}", currentPassword);
+        }catch (Exception e){
+            LOGGER.error("Failed to enter current password.", e);
+            throw e;
+        }
+    }
+
+    public void enterNewPassword(String newPassword){
+        try{
+            getNewPassword().sendKeys(newPassword);
+            LOGGER.info("Entered a new password. {}", newPassword);
+        }catch (Exception e){
+            LOGGER.error("Failed to enter new password.", e);
+            throw e;
+        }
+    }
+
+    public void enterConfirmPassword(String confirmPassword){
+        try{
+            getConfirmPassword().sendKeys(confirmPassword);
+            LOGGER.info("Entered a confirm password. {}", confirmPassword);
+        }catch (Exception e){
+            LOGGER.error("Failed to enter confirm password.", e);
+            throw e;
+        }
+    }
+
+    public void clickOnChangePasswordButton(){
+        try{
+            getChangePasswordButton().click();
+        }catch (Exception e){
+            LOGGER.error("Failed to click on Change password button.", e);
+            throw e;
+        }
+    }
+
+    public String readGettingStartedText(){
+        try{
+            return getGettingStartedText().getText();
+        }catch (Exception e){
+            LOGGER.error("Failed to read the text.", e);
+            throw e;
+        }
+    }
+
+    public void clickOnSupportButton(){
+        try{
+            getSupportButton().click();
+            LOGGER.info("Clicked on Support button.");
+        }catch (Exception e){
+            LOGGER.error("Failed to click on Support Button.", e);
+            throw e;
+        }
+    }
+
+    public void clickOnCloseButton(){
+        try{
+            scriptUtils.clickElementJS(getCloseButton());
+        }catch (Exception e){
+            LOGGER.error("Failed to click on close button.", e);
+            throw e;
+        }
+    }
+
+    public String readAboutHeaderText(){
+        try{
+            return getAboutTextHeader().getText();
+        }catch (Exception e){
+            LOGGER.error("Failed to read header text.", e);
+            throw e;
+        }
+    }
+
+    public void clickOnAboutButton(){
+        try{
+            getAboutButton().click();
+            LOGGER.info("Clicked on About button.");
+        }catch (Exception e){
+            LOGGER.error("Failed to click on About Button.", e);
+            throw e;
+        }
+    }
+
+    public void clickOnLogOutButton(){
+        try{
+            getLogOutButton().click();
+            LOGGER.info("Clicked on logout button.");
+        }catch (Exception e){
+            LOGGER.error("Failed to click on LogOut Button.", e);
+            throw e;
+        }
+    }
+
+    public void clickOnProfileName(){
+        try{
+            getProfileName().click();
+            LOGGER.info("Clicked on profile name.");
+        }catch (Exception e){
+            LOGGER.error("Failed to click on profile name.", e);
+            throw e;
+        }
+    }
 }
--- src/test/java/com/orangehrm/tests/DashboardPageTest.java
+++ src/test/java/com/orangehrm/tests/DashboardPageTest.java
@@ -1,6 +1,72 @@
 package com.orangehrm.tests;
 
 import com.orangehrm.basetest.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class DashboardPageTest extends BaseTest {
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUpPage(){
+        loginPage.enterUsername("Admin");
+        loginPage.enterPassword("admin123");
+        loginPage.clickOnLoginButton();
+    }
+
+    @Test(groups = { "sanity" })
+    public void testLogoutPage(){
+       dashboardPage.clickOnProfileName();
+       dashboardPage.clickOnLogOutButton();
+        Assert.assertEquals(loginPage.getPageTitle(), "OrangeHRM",
+                "\n Actual & Expected title do not match. \n");
+    }
+
+    @Test(groups = { "sanity" })
+    public void testAboutButton(){
+        dashboardPage.clickOnProfileName();
+        dashboardPage.clickOnAboutButton();
+        Assert.assertEquals(dashboardPage.readAboutHeaderText(),"About",
+                "\n Actual & Expected text do not match.\n");
+        dashboardPage.clickOnCloseButton();
+    }
+
+    @Test(groups = { "sanity" })
+    public void testSupportButton(){
+        dashboardPage.clickOnProfileName();
+        dashboardPage.clickOnSupportButton();
+        Assert.assertEquals(dashboardPage.readGettingStartedText(),"Getting Started with OrangeHRM",
+                "\n Actual & Expected text do not match. \n");
+    }
+
+    @Test(groups = { "sanity" })
+    public void testChangePasswordWithIncorrectCurrentPassword(){
+        dashboardPage.clickOnProfileName();
+        dashboardPage.clickOnChangePasswordButton();
+        dashboardPage.enterCurrentPassword("dsadsad");
+        dashboardPage.enterNewPassword("B35t4r1f1n@123");
+        dashboardPage.enterConfirmPassword("B35t4r1f1n@123");
+        logger.info("Please enter the correct current password.");
+    }
+
+    @Test(groups = { "sanity" })
+    public void testChangePasswordWithIncorrectConfirmPassword(){
+        dashboardPage.clickOnProfileName();
+        dashboardPage.clickOnChangePasswordButton();
+        dashboardPage.enterCurrentPassword("admin123");
+        dashboardPage.enterNewPassword("B35t4r1f1n@123");
+        dashboardPage.enterConfirmPassword("B35t4r1f1n@3");
+        Assert.assertEquals(dashboardPage.readErrorConfirmPassword(),"Passwords do not match",
+                "\n Actual & Expected error message do not match. \n");
+    }
+
+    @Test(groups = { "sanity" })
+    public void testChangePasswordWithValidCredentials(){
+        dashboardPage.clickOnProfileName();
+        dashboardPage.clickOnChangePasswordButton();
+        dashboardPage.enterCurrentPassword("admin123");
+        dashboardPage.enterNewPassword("admin123");
+        dashboardPage.enterConfirmPassword("admin123");
+        logger.info("Password updated successfully.");
+    }
 }
