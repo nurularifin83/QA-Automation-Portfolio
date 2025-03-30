095791835bff9bdd0364ea85dc87a7666c2a7011
Nurul Arifin <nurularifin837@gmail.com>
2025-03-30 17:02:31 +0700

    Updating frogot password test

--- src/main/java/com/orangehrm/pages/ForgotPasswordPage.java
+++ src/main/java/com/orangehrm/pages/ForgotPasswordPage.java
@@ -24,13 +24,84 @@ public class ForgotPasswordPage extends BasePage {
     @FindBy(xpath = "//h6[@class='oxd-text oxd-text--h6 orangehrm-forgot-password-title']")
     private WebElement headerText;
 
+    @FindBy(xpath = "//input[@name='username']")
+    private WebElement username;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    private WebElement resetPasswordButton;
+
+    @FindBy(xpath = "//span[@class='oxd-text oxd-text--span oxd-input-field-error-message oxd-input-group__message']")
+    private WebElement requiredErrorMessage;
+
+    @FindBy(xpath = "//button[@type='button']")
+    private WebElement cancelButton;
+
     // Getter
+    public WebElement getCancelButton(){
+        customWait.waitForElementToBeClickable(cancelButton);
+        return cancelButton;
+    }
+
+    public WebElement getRequiredErrorMessage(){
+        customWait.waitForVisibilityOfElement(requiredErrorMessage);
+        return requiredErrorMessage;
+    }
+
+    public WebElement getResetPassword(){
+        customWait.waitForElementToBeClickable(resetPasswordButton);
+        return resetPasswordButton;
+    }
+
+    public WebElement getUsername(){
+        customWait.waitForVisibilityOfElement(username);
+        return username;
+    }
+
     public WebElement getHeaderText(){
         customWait.waitForVisibilityOfElement(headerText);
         return headerText;
     }
 
     // Page actions
+    public void clickOnCancelButton(){
+        try{
+            getCancelButton().click();
+            LOGGER.info("Clicked on Cancel Button.");
+        }catch (Exception e){
+            LOGGER.error("Failed to click on Cancel Button.", e);
+            throw e;
+        }
+    }
+
+    public String readRequiredErrorMessage(){
+        try{
+            return getRequiredErrorMessage().getText();
+        }catch (Exception e){
+            LOGGER.error("Required Error Message element is not found.", e);
+            throw e;
+        }
+    }
+
+    public void clickOnResetPasswordButton(){
+        try{
+            getResetPassword().click();
+            LOGGER.info("Clicked on the Reset Password Button.");
+        }catch (Exception e){
+            LOGGER.error("Failed to click the Reset Password button.", e);
+            throw e;
+        }
+    }
+
+    public void enterUsername(String username){
+        try{
+            getUsername().sendKeys(username);
+            LOGGER.info("Entering a username: {}", username);
+        }catch (Exception e){
+            LOGGER.error("Failed to enter a username.", e);
+            throw e;
+        }
+    }
+
     public String readHeaderText(){
         try{
             return getHeaderText().getText();
--- src/test/java/com/orangehrm/tests/ForgotPasswordPageTest.java
+++ src/test/java/com/orangehrm/tests/ForgotPasswordPageTest.java
@@ -8,14 +8,37 @@ import org.testng.annotations.Test;
 public class ForgotPasswordPageTest extends BaseTest {
 
     @BeforeMethod(alwaysRun = true)
-    public void setUpPageForgotPassword(){
+    public void setUpPage(){
         homePage.clickOnForgotYourPassword();
     }
 
-    @Test(groups = { "sanity1" })
+    @Test(groups = { "sanity" })
     public void testForgotPasswordHeaderText(){
         Assert.assertEquals(forgotPasswordPage.readHeaderText(), "Reset Password",
                 "\n Actual & Expected Header Text do not match. \n");
+    }
+
+    @Test(groups = { "sanity" })
+    public void testLeaveUsernameEmpty(){
+        forgotPasswordPage.enterUsername("");
+        forgotPasswordPage.clickOnResetPasswordButton();
+        Assert.assertEquals(forgotPasswordPage.readRequiredErrorMessage(),"Required",
+                "\n Actual & Expected message do not match. \n");
+    }
+
+    @Test(groups = { "sanity" })
+    public void shouldAllowPasswordResetForValidUsername(){
+        forgotPasswordPage.enterUsername("Admin");
+        forgotPasswordPage.clickOnResetPasswordButton();
+        Assert.assertEquals(forgotPasswordPage.readHeaderText(),"Reset Password link sent successfully",
+                "\n Actual & Expected Header text do not match. \n");
+    }
+
+    @Test(groups = { "sanity" })
+    public void testCancelButton(){
+        forgotPasswordPage.clickOnCancelButton();
+        Assert.assertEquals(loginPage.getPageTitle(), "OrangeHRM",
+                "\n Actual & Expected text do not match. \n");
     }
 
 }
